2e29d7e53b05ceb164ceed921d8964cf1f303c44
John Clark <inindev@gmail.com>
2023-07-24 15:01:03 +0000

    remove extension from mk_extlinux
    Signed-off-by: John Clark <inindev@gmail.com>

--- scripts/file_fstab.sh
+++ scripts/file_fstab.sh
@@ -2,8 +2,8 @@ file_fstab() {
     local uuid="$1"
 
     cat <<-EOF
-	# if editing the device name for the root entry, it is necessary to
-	# regenerate the extlinux.conf file by running /boot/mk_extlinux.sh
+	# if editing the device name for the root entry, it is necessary
+	# to regenerate the extlinux.conf file by running /boot/mk_extlinux
 
 	# <device>					<mount>	<type>	<options>		<dump> <pass>
 	UUID=$uuid	/	ext4	errors=remount-ro	0      1
