2834a261d5be5c70b6a6bc45452509644a840b16
John Clark <inindev@gmail.com>
2023-08-07 11:45:48 +0000

    update for linux 6.5-rc5 release
    Signed-off-by: John Clark <inindev@gmail.com>

--- debian/gen_rock-5b.sh
+++ debian/gen_rock-5b.sh
@@ -9,7 +9,7 @@ main() {
       DEB_DIST: trixie
       HOSTNAME: rock5b-arm64
       DTB_FILE: rk3588-rock-5b.dtb
-      REL_URL:  https://github.com/inindev/rock-5b/releases/download/v12.0-6.5-rc1
+      REL_URL:  https://github.com/inindev/rock-5b/releases/download/v13-6.5-rc5
       FIRMWARE: microchip/mscc* nvidia/tegra??? r8a779x* rockchip rtl_bt rtl_nic
     '
 
