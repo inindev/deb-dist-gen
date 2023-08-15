f660edcc6fa1c1c0fdad9694e0188b1b9dcccfe2
John Clark <inindev@gmail.com>
2023-08-15 13:01:56 +0000

    update_all distributions script
    Signed-off-by: John Clark <inindev@gmail.com>

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 
 genall:
-	sh debian/genall.sh
-	sh dtb/genall.sh
+	sh ./debian/genall.sh
+	sh ./dtb/genall.sh
 
 clean:
-	rm -rf target_*
+	rm -rf ./target_*
+	sudo rm -rf 'dists'
+
+update:
+	sh ./update_all.sh
+
+
+.PHONY: genall clean update
 
---
+++ update_all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+rm -rf 'target_'*
+sh 'debian/genall.sh'
+sh 'dtb/genall.sh'
+
+sudo rm -rf 'dists'
+mkdir 'dists'
+
+targets='rockpi-4c-plus odroid-m1 nanopi-r5 radxa-e25 rock-5b nanopc-t6'
+for target in $targets; do
+    git -C 'dists' clone "git@github.com:inindev/$target"
+
+    [ 'rockpi-4c-plus' = "$target" ] && git -C 'dists/rockpi-4c-plus' checkout next
+    cp -r "target_$target/"* "dists/$target"
+done
+
